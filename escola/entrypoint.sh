bf959c04c0ebf58f3fc09f456f0400c3da1f5a54
Eduardo Basílio <eduardoafonsobasilio@gmail.com>
2020-09-03 05:21:03 -0300

    script waiting postgres [wip]

--- escola/entrypoint.sh
+++ escola/entrypoint.sh
@@ -1,3 +1,10 @@
+RETRIES=7
+
+until psql -h ${DB_HOST} -U ${DB_USER} -d ${DB_NAME} -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  sleep 5
+done
+
 pipenv run ./manage.py migrate &&
 pipenv run ./manage.py collectstatic --noinput &&
 
