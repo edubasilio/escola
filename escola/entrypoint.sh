ba5b825ac435c1994b1f2e587bd77df23bbba930
Eduardo Basílio <eduardoafonsobasilio@gmail.com>
2020-09-04 03:16:53 -0300

    created shell script waiting postgres

--- escola/entrypoint.sh
+++ escola/entrypoint.sh
@@ -1,13 +1,25 @@
-RETRIES=7
+#!/bin/bash
 
-until psql -h ${DB_HOST} -U ${DB_USER} -d ${DB_NAME} -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+### WAITING POSTGRES START ###
+RETRIES=7
+while [ "$RETRIES" -gt 0 ]
+do
   echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  PG_STATUS="$(pg_isready -h ${DB_HOST} -p 5432 -d ${DB_NAME} -U ${DB_USER})"
+  PG_EXIT=$(echo $?)
+  echo "Postgres Status: $PG_EXIT - $PG_STATUS"
+  if [ "$PG_EXIT" = "0" ];
+    then
+      RETRIES=0
+  fi
   sleep 5
 done
 
+### DJANGO MIGRATE AND COLLECT STATIC FILES ###
 pipenv run ./manage.py migrate &&
 pipenv run ./manage.py collectstatic --noinput &&
 
+### START WEBSERVER ###
 if [ ${MULTISTAGE} -eq "PROD" ]
 then
     pipenv run gunicorn -b 0.0.0.0:80 -w ${GUNICORN_WORKERS} -p /tmp/gunicorn_escola.pid escola.wsgi:application
