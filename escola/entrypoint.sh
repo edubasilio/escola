d2553ff1a096d561d90e3484a114e65ac25776ed
Eduardo Basílio <eduardoafonsobasilio@gmail.com>
2020-09-06 19:32:32 -0300

    fix start webserver in entryponint

--- escola/entrypoint.sh
+++ escola/entrypoint.sh
@@ -20,10 +20,9 @@ pipenv run ./manage.py migrate &&
 pipenv run ./manage.py collectstatic --noinput &&
 
 ### START WEBSERVER ###
-if [ ${MULTISTAGE} -eq "PROD" ]
-then
+if [ ${MULTISTAGE} = "PROD" ]; then
     pipenv run gunicorn -b 0.0.0.0:80 -w ${GUNICORN_WORKERS} -p /tmp/gunicorn_escola.pid escola.wsgi:application
 else
-    pipenv run gunicorn -D -b 0.0.0.0:80 -w ${GUNICORN_WORKERS} -p /tmp/gunicorn_escola.pid escola.wsgi:application && 
-    pipenv run ./manage.py runserver 0.0.0.0:8000
+    pipenv run gunicorn -D -b 0.0.0.0:80 -w 2 -p /tmp/gunicorn_escola.pid escola.wsgi:application && 
+    pipenv run ./manage.py runserver 0:8000
 fi
